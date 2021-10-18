b92347a642f1bfc5aed395509c8b2460c8e68bf6
aasmith33 <76235968+aasmith33@users.noreply.github.com>
2021-10-18 15:29:01 -0400

    Add project files.

---
+++ AbigailSmith-CPT-185-Chapter-4/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace AbigailSmith_CPT_185_Chapter_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstFrom = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstTo = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtConverted = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a distance to convert:";
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Location = new System.Drawing.Point(279, 39);
+            this.txtDistance.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(256, 27);
+            this.txtDistance.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.txtDistance, "Enter distance");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstFrom);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(20, 82);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(261, 173);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "From";
+            // 
+            // lstFrom
+            // 
+            this.lstFrom.FormattingEnabled = true;
+            this.lstFrom.ItemHeight = 19;
+            this.lstFrom.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.lstFrom.Location = new System.Drawing.Point(55, 39);
+            this.lstFrom.Margin = new System.Windows.Forms.Padding(4);
+            this.lstFrom.Name = "lstFrom";
+            this.lstFrom.Size = new System.Drawing.Size(149, 80);
+            this.lstFrom.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstTo);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(304, 82);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(261, 173);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "To";
+            // 
+            // lstTo
+            // 
+            this.lstTo.FormattingEnabled = true;
+            this.lstTo.ItemHeight = 19;
+            this.lstTo.Items.AddRange(new object[] {
+            "Inches",
+            "Feet",
+            "Yards"});
+            this.lstTo.Location = new System.Drawing.Point(55, 39);
+            this.lstTo.Margin = new System.Windows.Forms.Padding(4);
+            this.lstTo.Name = "lstTo";
+            this.lstTo.Size = new System.Drawing.Size(149, 80);
+            this.lstTo.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 274);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Converted Distance:";
+            // 
+            // txtConverted
+            // 
+            this.txtConverted.Enabled = false;
+            this.txtConverted.Location = new System.Drawing.Point(279, 271);
+            this.txtConverted.Margin = new System.Windows.Forms.Padding(4);
+            this.txtConverted.Name = "txtConverted";
+            this.txtConverted.Size = new System.Drawing.Size(256, 27);
+            this.txtConverted.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(85, 328);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(140, 38);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "&Convert";
+            this.toolTip1.SetToolTip(this.btnConvert, "Click to convert");
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(380, 328);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(140, 38);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.toolTip1.SetToolTip(this.btnExit, "Click to exit");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(232, 328);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(140, 38);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "C&lear";
+            this.toolTip1.SetToolTip(this.btnClear, "Click to convert");
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnConvert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(599, 406);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtConverted);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Distance Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lstFrom;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConverted;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ AbigailSmith-CPT-185-Chapter-4/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbigailSmith_CPT_185_Chapter_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtDistance.Focus();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {   //declaring variables
+            string fromDistance, toDistance;
+            try
+            {
+                double distance = double.Parse(txtDistance.Text);
+                if (distance > 0)
+                {   
+                    // declaring variables
+                    fromDistance = lstFrom.SelectedIndex.ToString();
+                    toDistance = lstTo.SelectedIndex.ToString();
+                    string feet = " ft";
+                    string yard = " yd";
+                    string inches = " in";
+                    // convert units
+                    if (fromDistance == "2" && toDistance == "1") // from yards to feet
+                        txtConverted.Text = (distance * 3).ToString() + feet;
+                    else if (fromDistance == "1" && toDistance == "2") // from feet to yards
+                        txtConverted.Text = (distance / 3).ToString() + yard;
+                    else if (fromDistance == "2" && toDistance == "0") // from yards to inches
+                        txtConverted.Text = (distance * 36).ToString() + inches;
+                    else if (fromDistance == "0" && toDistance == "2") // from inches to yards
+                        txtConverted.Text = (distance / 36).ToString() + yard;
+                    else if (fromDistance == "1" && toDistance == "0") // from feet to inches
+                        txtConverted.Text = (distance * 12).ToString() + inches;
+                    else if (fromDistance == "0" && toDistance == "1") // from inches to feet
+                        txtConverted.Text = (distance / 12).ToString() + feet;
+                    else if (fromDistance == toDistance && toDistance == "0") // from inches to inches
+                        txtConverted.Text = distance.ToString() + " " + inches;
+                    else if (fromDistance == toDistance && toDistance == "1") // from feet to feet
+                        txtConverted.Text = distance.ToString() + " " + feet;
+                    else if (fromDistance == toDistance && toDistance == "2") // from yards to yards
+                        txtConverted.Text = distance.ToString() + " " + yard;
+                }
+                
+                else MessageBox.Show("Negative Number"); // error message 
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message); //error message 
+                txtDistance.Focus(); // puts insertion point back to the beginning
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {   // clears form when clicked
+            txtDistance.Clear();
+            txtConverted.Clear();
+            lstFrom.ClearSelected();
+            lstTo.ClearSelected();
+            txtDistance.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {   // closes form when clicked
+            Close();
+        }
+    }
+}
